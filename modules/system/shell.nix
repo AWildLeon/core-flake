5153adb4420579d3054f84102efbdc10807a3d76
Leon Hubrich <git@leon-hubrich.de>
2026-01-17 12:56:30 +0100

    refactor(shell): Streamline shell configuration and remove custom fastfetch setup

--- modules/system/shell.nix
+++ modules/system/shell.nix
@@ -1,41 +1,29 @@
-{ pkgs, config, lib, self, ... }:
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
 
 let
   theme_omz = builtins.fetchurl {
     url = "https://zsh.onlh.de/theme.omp.json";
     sha256 = "1jd355hilldj4ncf0h28n70qwx43zddzn5xdxamc2y6dmlmxh79c";
   };
-  LHFastfetchLogo =
-    builtins.path { path = self + "/resources/logo_fastfetch.ansi"; };
-  lhShellTools = pkgs.stdenv.mkDerivation {
-    pname = "lh-shell-tools";
-    version = "1.0";
-    src = ./shell-tools;
 
-    # If your repo preserves +x, you can skip chmod and just use install -Dm755.
-    installPhase = ''
-      mkdir -p $out/bin
-      # take only regular files in the root of src; adjust as needed
-      for f in "$src"/*; do
-        [ -f "$f" ] || continue
-        install -Dm755 "$f" "$out/bin/$(basename "$f")"
-      done
-    '';
-  };
+  cfg = config.lh.system.shell;
 in
 {
   options.lh.system.shell = {
-    enable = lib.mkEnableOption
-      "Enable Zsh as the default shell with Oh My Zsh and enhancements.";
+    enable = lib.mkEnableOption "Enable Zsh as the default shell with Oh My Zsh and enhancements.";
     LHLogo = lib.mkOption {
       type = lib.types.bool;
       default = true;
-      description =
-        "Whether to show the LH Logo in the terminal prompt instead of the NixOS logo.";
+      description = "Whether to show the LH Logo in the terminal prompt instead of the NixOS logo.";
     };
   };
 
-  config = lib.mkIf config.lh.system.shell.enable {
+  config = lib.mkIf cfg.enable {
     programs.zsh = {
       enable = true;
       enableBashCompletion = true;
@@ -47,15 +35,17 @@ in
         #ls = "eza --icons always";
         "cd.." = "cd ..";
       };
-      interactiveShellInit =
-        if config.lh.system.shell.LHLogo then ''
-          fastfetch -l "${LHFastfetchLogo}"
-        '' else ''
-          fastfetch
-        '';
+      interactiveShellInit = ''
+        fastfetch
+      '';
       ohMyZsh = {
         enable = true;
-        plugins = [ "git" "sudo" "docker" "docker-compose" ];
+        plugins = [
+          "git"
+          "sudo"
+          "docker"
+          "docker-compose"
+        ];
         #"eza"
       };
       vteIntegration = true;
